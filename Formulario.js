4200c183684a98d799915ece242c4e5c369ff5a5
NayeliGS <gsnayeli8@gmail.com>
2023-07-30 13:06:36 -0600

    2. Return de objeto desde firebase

--- Formulario.js
+++ Formulario.js
@@ -23,29 +23,51 @@ button.addEventListener('click', (event) => {
     console.log(persona)
 
     createData(persona)
+
+    reciveData();
+
     
+
+
 });
 
-/////////////////////////////// DATOS ALMACENADOS EN FIREBASE //////////////////////////////////////
 
 const createData = async(persons) => {
-
+   
     const dataBase = await fetch("https://predesafiojs-default-rtdb.firebaseio.com/.json", { 
         method: "POST",
         headers: {
             "Content-type": "application/json;charset=UTF-8"
         },
-        body: JSON.stringify(persona),
+        body: JSON.stringify(persons),
     });
 }; 
 
-// const consultarTodos = async() => {
+
+
+const reciveData = async() => {
+
+    const recive = await fetch("https://predesafiojs-default-rtdb.firebaseio.com/.json", { 
+        method: "GET",
+    });
+    const data = await recive.json();
+
+    const array = Object.entries(data);
+
+    const infoCard = array.map((item)=>{
+        console.log(item[0]);
+        const object = {
+            id:item[0],
+            age:item[1].age,
+            name: item[1].name
+        }
+        return object;
+    })
+
+    console.log(infoCard);
     
-//     pintarDom(ArrayTodod)
-// }
-
-
-
+}; 
+reciveData();
 
 
 
